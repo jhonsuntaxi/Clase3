9c5c4bdc9a504e4c3536d6d2ad54347d25fc6345
jhonsuntaxi <73371683+jhonsuntaxi@users.noreply.github.com>
2020-10-29 11:35:31 -0500

    Agregar archivos de proyecto.

---
+++ Clase3/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Clase3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVentana2_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            string user_val = "uisrael";
+            string pass_val = "uisrael2020";
+            
+            await Navigation.PushAsync(new Page1(user, pass));
+        }
+    }
+}
